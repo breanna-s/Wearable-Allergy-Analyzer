bfcd066b0419fdc11307a2084f3e6d8c359ec7a8
breanna-s <bslessma@vols.utk.edu>
2025-01-14 13:46:25 -0500

    Updated Index Style
    Merged an older index design with the new page format

--- AllergyAlarm/app/index.tsx
+++ AllergyAlarm/app/index.tsx
@@ -1,27 +1,60 @@
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { useRouter } from 'expo-router';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const Separator = () => <View style={styles.separator} />;
 
 const Index = () => {
   const router = useRouter();
 
   return (
-    <View style={styles.container}>
-      <Button title="Login" onPress={() => router.push('/(auth)/Login')} />
-      <Button title="Sign Up" onPress={() => router.push('/(auth)/SignUp')} />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.title}>Welcome to the Auth Group</Text>
+        </View>
+        <Separator />
+        <View style={styles.fixToText}>
+          <Button title="Log In"
+           color='#FDBAC1' 
+           onPress={() => router.push('/(auth)/Login')} />
+        </View>
+        <View style={styles.fixToText}>
+          <Button title="Sign Up" 
+           color='#FDBAC1'
+           onPress={() => router.push('/(auth)/SignUp')} />
+        </View>
+        {/* </View> */}
+      </SafeAreaView> 
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // Take up the full height of the screen
-    justifyContent: 'center', // Center vertically
-    alignItems: 'center', // Center horizontally
-    backgroundColor: '#000', // Black background for consistency
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+    marginVertical: 10,
   },
-  button: {
-    marginVertical: 10, // Add spacing between buttons
+  title: {
+    color: '#555555',
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+   // flexDirection: 'row',
+    color: '#FDBAC1',
+    marginVertical:10,
+    paddingHorizontal: 75,
+    textAlign: 'center',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 10,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
 
